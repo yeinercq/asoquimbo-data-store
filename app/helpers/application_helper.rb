85a7178ccd03dd5e579dce26283ef00d26c8a89b
Yeiner CQ <ycarvajalq@gmail.com>
2026-04-10 15:02:53 -0500

    feat: add initial message to form errors messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def form_error_notification(object)
     if object.errors.any?
       tag.div class: "alert alert-danger alert-dismissible fade show", role: "alert" do
-        object.errors.full_messages.to_sentence.capitalize
+        t("common.form.error_message") + object.errors.full_messages.to_sentence.capitalize
       end
     end
   end
