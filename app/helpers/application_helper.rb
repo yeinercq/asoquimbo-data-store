246f22af8c43ebeb3a2fe8ae49da8d017e6be15f
Yeiner CQ <ycarvajalq@gmail.com>
2026-04-10 15:29:22 -0500

    refacto: add application_helper method to options from model enums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
   def form_error_notification(object)
     if object.errors.any?
       tag.div class: "alert alert-danger alert-dismissible fade show", role: "alert" do
-        t("common.form.error_message") + object.errors.full_messages.to_sentence.capitalize
+        I18n.t("common.form.error_message") + object.errors.full_messages.to_sentence.capitalize
       end
     end
+  end
+
+  def enum_options_for_select(model, enum, selected = nil)
+    model.send(enum.to_s).map { |key, _| [ key.to_s.humanize, key ] }
   end
 end
