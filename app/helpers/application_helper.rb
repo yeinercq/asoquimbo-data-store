b9e7b67327f6f863e26925c6e6bb5a0d8e213700
Yeiner CQ <ycarvajalq@gmail.com>
2026-04-14 21:53:51 -0500

    feat: add link_to_active helper to nav item links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,17 @@ module ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.prepend "flash_notifications", partial: "layouts/flash_notifications"
   end
+
+  # Adds active class to links
+  def link_to_active(text = nil, path = nil, **options, &block)
+    link = block_given? ? text : path
+
+    options[:class] = class_names(options[:class], "active") if current_page? link
+
+    if block_given?
+      link_to text, options, &block
+    else
+      link_to text, path, options
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "social_ecological_characterizations#index"
+  root "pages#home"
 
   resources :social_ecological_characterizations do
       collection do
