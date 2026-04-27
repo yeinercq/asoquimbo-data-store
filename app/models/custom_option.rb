0a081d7563a11fe907b578d3e138cb6a00f73631
Yeiner CQ <ycarvajalq@gmail.com>
2026-04-26 23:29:58 -0500

    refactor: move custom_select_list to user nav button

--- app/models/custom_option.rb
+++ app/models/custom_option.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: custom_options
+#
+#  id                    :bigint           not null, primary key
+#  custom_option_list_id :bigint           not null
+#  name                  :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
 class CustomOption < ApplicationRecord
   belongs_to :custom_option_list
 
