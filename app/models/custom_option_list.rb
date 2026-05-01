a550383acc37efc7442dfa3671f5d41ac3751a33
Yeiner CQ <ycarvajalq@gmail.com>
2026-05-01 00:22:56 -0500

    feat: add custo_options destroy validations

--- app/models/custom_option_list.rb
+++ app/models/custom_option_list.rb
@@ -11,12 +11,12 @@
 class CustomOptionList < ApplicationRecord
   belongs_to :custom_select_list
   has_many :custom_options, dependent: :destroy
-
   accepts_nested_attributes_for :custom_options, reject_if: :all_blank, allow_destroy: true
 
   validates :model_field, presence: true
   validate :ensure_at_least_one_custom_option, on: [ :create, :update ]
   validate :exists_one_model_associated, on: :is_destroyed
+  validate :custom_options_not_in_use, on: :update
 
   scope :ordered, -> { order(id: :desc) }
 
@@ -32,6 +32,20 @@ class CustomOptionList < ApplicationRecord
     model = custom_select_list.model_name_association.camelize.constantize rescue nil
     if model.exists?
       errors.add(:base, "No se puede eliminar esta lista porque hay una #{I18n.t("activerecord.models.#{custom_select_list.model_name_association}.one")} asociada a ella.")
+    end
+  end
+
+  def custom_options_not_in_use
+    model_class = custom_select_list.model_name_association.camelize.constantize rescue nil
+    return unless model_class
+
+    custom_options.each do |option|
+      next unless option.marked_for_destruction?
+
+      if model_class.exists?(model_field => option.id)
+        errors.add(:base, "No es posible eliminar las opciones porque están siendo utilizadas en una #{I18n.t("activerecord.models.#{custom_select_list.model_name_association}.one")}")
+        break
+      end
     end
   end
 end
