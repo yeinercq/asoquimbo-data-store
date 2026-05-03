b3d0eb7ab2d3425fc69940d7ec60c11e22a88111
Yeiner CQ <ycarvajalq@gmail.com>
2026-05-03 18:14:50 -0500

    feat: add update action to collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,4 +1,5 @@
 class CollaboratorsController < ApplicationController
+  before_action :set_collaborator, only: [ :edit, :update, :destroy ]
   def index
     @collaborators = User.ordered_by_name
   end
@@ -11,8 +12,8 @@ class CollaboratorsController < ApplicationController
     @collaborator = User.build(user_params)
     if @collaborator.save
       respond_to do |format|
-        format.html { redirect_to collaborators_path, notice: "Collaborador creado exitosamente." }
-        format.turbo_stream { flash.now[:notice] = "Collaborador creado exitosamente." }
+        format.html { redirect_to collaborators_path, notice: "Colaborador creado exitosamente." }
+        format.turbo_stream { flash.now[:notice] = "Colaborador creado exitosamente." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -23,12 +24,24 @@ class CollaboratorsController < ApplicationController
   end
 
   def update
+    if @collaborator.update(user_params)
+      respond_to do |format|
+        format.html { redirect_to collaborators_path, notice: "Colaborador editado exitosamente." }
+        format.turbo_stream { flash.now[:notice] = "Colaborador editado exitosamente." }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
 
   private
+
+  def set_collaborator
+    @collaborator = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation, :role)
