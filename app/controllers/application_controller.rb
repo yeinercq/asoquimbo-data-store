5fe78c4655115276f512a6ab7b20b9230c37d56c
Yeiner CQ <ycarvajalq@gmail.com>
2026-07-15 14:56:13 -0500

    refactor: change devide controller views to make home page visible with no logged user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
   before_action :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
